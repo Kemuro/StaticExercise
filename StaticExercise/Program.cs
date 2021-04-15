1d353e381772f6983343980b069339aca7e54104
Kemuro <antonkemuro@gmail.com>
2021-04-15 11:27:12 -0500

    created StaticExercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.FahrenheitToCelsius(200);
+            Console.WriteLine();
+            TempConverter.CelsiusToFahrenheit(150);
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static void FahrenheitToCelsius(double temperature)
+        {
+            double celsius = (temperature - 32) / 1.8000;
+            Console.WriteLine($"{temperature} F is {celsius} C.");
+        }
+        public static void CelsiusToFahrenheit(double temperature)
+        {
+            double fahrenheit = (temperature * 1.8000) + 32;
+            Console.WriteLine($"{temperature} C is {fahrenheit} F.");
+        }
+    }
+}
